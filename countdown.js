e04d3b1b403f1137df893d15f17f8b14fd9eaf17
AstrocatFreitag <astro@thesov.net>
2018-06-21 19:09:05 -0400

    good to go, more or less

--- countdown.js
+++ countdown.js
@@ -1,15 +1,12 @@
-var countDownDate = new Date("Sep 5, 2018 15:37:25").getTime();
+var seconds = 0;
+var minute = Math.floor(Math.random() * 60);
 
-var x = setInterval(function() {
-	var now = new Date().getTime();
-	var distance = countDownDate - now;
-	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-	document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-	if (distance < 0) {
-		clearInterval(x);
-		document.getElementById("countdown").innerHTML = "EXPIRED";
+var y = setInterval(function() {
+	var second = Math.floor(Math.random() * 60);
+	seconds = seconds + 1;
+	var modsec = seconds % 60;
+	if (modsec === 0) {
+		minute = Math.floor(Math.random() * 60);
 	}
+	document.getElementById("countdown").innerHTML = second + ":" + minute;
 }, 1000);
