6e1acc91a43d4757d3e96c901cb91822702b65bc
AstrocatFreitag <astro@thesov.net>
2018-06-22 17:58:40 -0400

    countdown.js: made the second counter change to a zero padded number between 0 and 60 at a random interval between 0 and 60 seconds

--- countdown.js
+++ countdown.js
@@ -1,14 +1,16 @@
 var seconds = 0;
 var minute = Math.floor(Math.random() * 60);
 var padMinute = String("00" + minute).slice(-2);
+var interval = Math.floor(Math.random() * 60);
 
 var WTF = setInterval(function() {
 	var second = Math.floor(Math.random() * 60);
 	var padSecond = String("00" + second).slice(-2);
-	var modsec = seconds % 60;
+	var modsec = seconds % interval;
 	if (modsec === 0) {
 		minute = Math.floor(Math.random() * 60);
 		padMinute = String("00" + minute).slice(-2);
+		interval = Math.floor(Math.random() * 60);
 	}
 	document.getElementById("countdown").innerHTML = padSecond + ":" + padMinute;
 	seconds = seconds + 1;
