5ff3b6cb75f3a07f577198bf77de1c84192fd22e
AstrocatFreitag <astro@thesov.net>
2018-12-27 13:20:18 -0500

    Update countdown.js
    well that didn't work let's try fixing the second counter again

--- countdown.js
+++ countdown.js
@@ -1,21 +1,17 @@
 var seconds = 0;
+var interval = Math.floor(Math.random() * 60);*
 var minute = Math.floor(Math.random() * 60);
 var padMinute = String("00" + minute).slice(-2);
-var interval = Math.floor(Math.random() * 60);
 
 var WTF = setInterval(function() {
 	var second = Math.floor(Math.random() * 60);
 	var padSecond = String("00" + second).slice(-2);
-	var modsec = seconds % interval;
 	
-	if (modsec === 0) {
+	if (seconds == interval) {
+		seconds = 0;
+		interval = Math.floor(Math.random() * 60);
 		minute = Math.floor(Math.random() * 60);
 		padMinute = String("00" + minute).slice(-2);
-		interval = Math.floor(Math.random() * 60);
-	}
-	
-	if (seconds == 61) {
-		seconds = Math.floor(Math.random() * 60);
 	} else {
 		seconds = seconds + 1;
 	}
