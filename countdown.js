80d95b626886ad70aa8f9cf8dc378291c529bc6e
AstrocatFreitag <astro@thesov.net>
2018-12-27 12:43:06 -0500

    Update countdown.js
    the old method of constantly incrementing seconds and dividing that number by some randomly generated interval fails to change the second value on the counter if the seconds counter gets too big.
    
    attempting to fix that by checking if the seconds counter gets to 61, and if it does reset it to a value between 0 and 59.  else add one second to the seconds counter.

--- countdown.js
+++ countdown.js
@@ -7,11 +7,19 @@ var WTF = setInterval(function() {
 	var second = Math.floor(Math.random() * 60);
 	var padSecond = String("00" + second).slice(-2);
 	var modsec = seconds % interval;
+	
 	if (modsec === 0) {
 		minute = Math.floor(Math.random() * 60);
 		padMinute = String("00" + minute).slice(-2);
 		interval = Math.floor(Math.random() * 60);
 	}
+	
+	if (seconds == 61) {
+		seconds = Math.floor(Math.random() * 60);
+	} else {
+		seconds = seconds + 1;
+	}
+	
 	document.getElementById("countdown").innerHTML = padSecond + ":" + padMinute;
-	seconds = seconds + 1;
+	
 }, 1000);
